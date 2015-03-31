c46665800542014eefd7384111f5922b7225b55c
Theo Spears <theo@Theos-MacBook-Pro.local>
2015-03-30 21:40:58 -0700

    Initial Commit

---
+++ TodayLight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayLight
+//
+//  Created by Theo Spears on 3/30/15.
+//  Copyright (c) 2015 Theo Spears. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
